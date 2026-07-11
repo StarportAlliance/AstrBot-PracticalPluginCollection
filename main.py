6c4448ad73e5425e5362b46bdecbeadf45de2f5c
QingFeng-awa <qingfeng_awa@163.com>
2026-07-11 21:03:16 +0800

    refactor: 改进 Command Handler 事件过滤器传参

--- main.py
+++ main.py
@@ -41,12 +41,11 @@ class PracticalPluginCollection(Star):
     async def terminate(self):
         logger.info("插件已终止。")
 
-    def _event_filter(self, event: AstrMessageEvent, whitelist_config: dict) -> bool:
+    def _event_filter(self, event: AstrMessageEvent) -> bool:
         """事件过滤器。
 
         Args:
             event (AstrMessageEvent): 事件对象。
-            whitelist_config (dict): 插件白名单配置对象。
 
         Returns:
             bool: 如果通过检查则返回 True，否则返回 False。
@@ -58,6 +57,7 @@ class PracticalPluginCollection(Star):
         user_id = event.get_sender_id()
         group_id = event.get_group_id()
         request_type = event.get_message_type().value
+        whitelist_config = self.config["Whitelist"]
         match request_type:
             case "GroupMessage":  # 群聊
                 return group_id in whitelist_config["WhitelistGroups"]
@@ -83,7 +83,7 @@ class PracticalPluginCollection(Star):
     @ban.command("add")
     async def add_ban(self, event: AstrMessageEvent, user_id: str, reason: str = ""):
         """新增封禁用户。"""
-        if not self._event_filter(event, self.config["Whitelist"]):
+        if not self._event_filter(event):
             return
         yield self.global_entry.ban.add(event, user_id, reason)
 
@@ -92,7 +92,7 @@ class PracticalPluginCollection(Star):
     @ban.command("remove")
     async def remove_ban(self, event: AstrMessageEvent, user_id: str):
         """解封给定用户。"""
-        if not self._event_filter(event, self.config["Whitelist"]):
+        if not self._event_filter(event):
             return
         yield self.global_entry.ban.remove(event, user_id)
 
@@ -101,14 +101,14 @@ class PracticalPluginCollection(Star):
     @ban.command("list")
     async def list_ban(self, event: AstrMessageEvent):
         """列出所有封禁用户。"""
-        if not self._event_filter(event, self.config["Whitelist"]):
+        if not self._event_filter(event):
             return
         yield self.global_entry.ban.list(event)
 
     @filter.platform_adapter_type(filter.PlatformAdapterType.AIOCQHTTP)
     async def group_request_review(self, event: AstrMessageEvent):
         """加群请求自动审核模块事件接收器。"""
-        if not self._event_filter(event, self.config["Whitelist"]):
+        if not self._event_filter(event):
             return
         if not self.module_config["GroupRequestReview"]["Enable"]:
             return
@@ -118,7 +118,7 @@ class PracticalPluginCollection(Star):
     @filter.command("like")
     async def like(self, event: AstrMessageEvent, times: int = 10):
         """赞我！"""
-        if not self._event_filter(event, self.config["Whitelist"]):
+        if not self._event_filter(event):
             return
         if not self.module_config["EnableSendLike"]:
             return
