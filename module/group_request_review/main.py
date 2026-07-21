569a210c48b7e6244ef8a1d0ad410fdfc70a3893
QingFeng-awa <qingfeng_awa@163.com>
2026-07-21 13:37:41 +0800

    fix(group_request_review): 补充对加群请求审核的异常处理

--- module/group_request_review/main.py
+++ module/group_request_review/main.py
@@ -65,147 +65,155 @@ class GroupRequestReview:
         Args:
             event (AstrMessageEvent): 加群请求事件。
         """
-        # 基础事件筛选
-        if not event_filter(event, "request", "group", "add"):
-            return
+        try:
+            # 基础事件筛选
+            if not event_filter(event, "request", "group", "add"):
+                return
 
-        # 解析事件数据
-        raw_message = cast(dict, event.message_obj.raw_message)
-        group_id = str(raw_message["group_id"])
-        user_id = str(raw_message["user_id"])
-        raw_comment = str(raw_message["comment"])
-        if "\n答案：" in raw_comment:
-            input_answer = raw_comment.split("\n答案：", 1)[1]
-        else:
-            input_answer = raw_comment
-            logger.warning(
-                f"未能判断用户 {user_id} 加群实际输入答案，将使用原始内容作为输入：`{raw_comment}`。"
+            # 解析事件数据
+            raw_message = cast(dict, event.message_obj.raw_message)
+            group_id = str(raw_message["group_id"])
+            user_id = str(raw_message["user_id"])
+            raw_comment = str(raw_message["comment"])
+            if "\n答案：" in raw_comment:
+                input_answer = raw_comment.split("\n答案：", 1)[1]
+            else:
+                input_answer = raw_comment
+                logger.warning(
+                    f"未能判断用户 {user_id} 加群实际输入答案，将使用原始内容作为输入：`{raw_comment}`。"
+                )
+            request_flag = str(raw_message["flag"])
+            logger.info(
+                f"收到用户 {user_id} 来自群 {group_id} 的加群请求，答案：`{input_answer}`。"
             )
-        request_flag = str(raw_message["flag"])
-        logger.info(
-            f"收到用户 {user_id} 来自群 {group_id} 的加群请求，答案：`{input_answer}`。"
-        )
 
-        # 加载审核策略
-        review_strategies = cast(list[dict], self._module_config["ReviewStrategy"])
-        group_config = None
-        for cfg in review_strategies:
-            if cfg["__template_key"] == "Dedicated" and cfg["TargetGroup"] == group_id:
-                group_config = cfg
-                break
-        if group_config is None:
+            # 加载审核策略
+            review_strategies = cast(list[dict], self._module_config["ReviewStrategy"])
+            group_config = None
             for cfg in review_strategies:
-                if cfg["__template_key"] == "Common":
+                if (
+                    cfg["__template_key"] == "Dedicated"
+                    and cfg["TargetGroup"] == group_id
+                ):
                     group_config = cfg
                     break
-        if group_config is None:
-            logger.warning(
-                f"未能为群 {group_id} 找到可用的审核策略，请检查你的插件配置是否已正确配置对应策略。此次加群请求将被忽略。"
-            )
-            return
+            if group_config is None:
+                for cfg in review_strategies:
+                    if cfg["__template_key"] == "Common":
+                        group_config = cfg
+                        break
+            if group_config is None:
+                logger.warning(
+                    f"未能为群 {group_id} 找到可用的审核策略，请检查你的插件配置是否已正确配置对应策略。此次加群请求将被忽略。"
+                )
+                return
 
-        # 主要判断逻辑部分
-        if group_config["UseBanlist"]:
-            if self._ban_system.core.check_user_is_banned(user_id):
-                logger.info(f"用户 {user_id} 已被封禁，将拒绝其加群请求。")
-                await self._handle_request(
-                    event,
-                    request_flag,
-                    False,
-                    self._message_template.get_msg_template(
-                        "GroupRequestReview", "RejectByBanned"
-                    ),
-                )
-                return
-        time_limit = cast(dict, group_config["TimeLimit"])
-        if time_limit["Enable"]:
-            statistical_time = int(time_limit["StatisticalTime"])
-            total_attempt = int(time_limit["TotalAttempt"])
-            cutoff_time = (
-                datetime.now() - timedelta(minutes=statistical_time)
-            ).strftime("%Y-%m-%d %H:%M:%S")
-            recent_requests = await self._log.get_requests(user_id, cutoff_time)
-            if len(recent_requests) >= total_attempt:
-                logger.info(
-                    f"用户 {user_id} 在 {statistical_time} 分钟内请求 {len(recent_requests)} 次，超过上限 {total_attempt}，将拒绝其加群请求。"
-                )
-                earliest_request_time = min(
-                    datetime.strptime(req["request_time"], "%Y-%m-%d %H:%M:%S")
-                    for req in recent_requests
-                )
-                remaining_delta = (
-                    earliest_request_time
-                    + timedelta(minutes=statistical_time)
-                    - datetime.now()
-                )
-                remaining_time = math.ceil(remaining_delta.total_seconds() / 60)
-                await self._handle_request(
-                    event,
-                    request_flag,
-                    False,
-                    self._message_template.get_msg_template(
-                        "GroupRequestReview",
-                        "RejectByRateLimit",
-                        remaining_time=str(remaining_time),
-                    ),
-                )
-                return
-            # 即使没有通过下面的其他限制也需要计入请求次数
-            await self._log.add_request(user_id)
-        level_limit = cast(dict, group_config["LevelLimit"])
-        if level_limit["MinLevel"] > 0:
-            member_info = await ProtocolEndApi.get_stranger_info(
-                event, int(user_id), True
-            )
-            if member_info["isHideQQLevel"]:
-                if not level_limit["SkipInvalidLevel"]:
-                    logger.info(f"用户 {user_id} 隐藏了 QQ 等级，将拒绝其加群请求。")
+            # 主要判断逻辑部分
+            if group_config["UseBanlist"]:
+                if self._ban_system.core.check_user_is_banned(user_id):
+                    logger.info(f"用户 {user_id} 已被封禁，将拒绝其加群请求。")
                     await self._handle_request(
                         event,
                         request_flag,
                         False,
                         self._message_template.get_msg_template(
-                            "GroupRequestReview", "RejectByInvalidLevel"
+                            "GroupRequestReview", "RejectByBanned"
                         ),
                     )
                     return
-                logger.debug(
-                    f"用户 {user_id} 隐藏了 QQ 等级，但插件配置中启用了跳过无效等级，将绕过等级限制。"
+            time_limit = cast(dict, group_config["TimeLimit"])
+            if time_limit["Enable"]:
+                statistical_time = int(time_limit["StatisticalTime"])
+                total_attempt = int(time_limit["TotalAttempt"])
+                cutoff_time = (
+                    datetime.now() - timedelta(minutes=statistical_time)
+                ).strftime("%Y-%m-%d %H:%M:%S")
+                recent_requests = await self._log.get_requests(user_id, cutoff_time)
+                if len(recent_requests) >= total_attempt:
+                    logger.info(
+                        f"用户 {user_id} 在 {statistical_time} 分钟内请求 {len(recent_requests)} 次，超过上限 {total_attempt}，将拒绝其加群请求。"
+                    )
+                    earliest_request_time = min(
+                        datetime.strptime(req["request_time"], "%Y-%m-%d %H:%M:%S")
+                        for req in recent_requests
+                    )
+                    remaining_delta = (
+                        earliest_request_time
+                        + timedelta(minutes=statistical_time)
+                        - datetime.now()
+                    )
+                    remaining_time = math.ceil(remaining_delta.total_seconds() / 60)
+                    await self._handle_request(
+                        event,
+                        request_flag,
+                        False,
+                        self._message_template.get_msg_template(
+                            "GroupRequestReview",
+                            "RejectByRateLimit",
+                            remaining_time=str(remaining_time),
+                        ),
+                    )
+                    return
+                # 即使没有通过下面的其他限制也需要计入请求次数
+                await self._log.add_request(user_id)
+            level_limit = cast(dict, group_config["LevelLimit"])
+            if level_limit["MinLevel"] > 0:
+                member_info = await ProtocolEndApi.get_stranger_info(
+                    event, int(user_id), True
                 )
-            elif member_info["qqLevel"] < level_limit["MinLevel"]:
-                logger.info(
-                    f"用户 {user_id} 等级 {member_info['qqLevel']} 低于要求的最小等级 {level_limit['MinLevel']}，将拒绝其加群请求。"
-                )
+                if member_info["isHideQQLevel"]:
+                    if not level_limit["SkipInvalidLevel"]:
+                        logger.info(
+                            f"用户 {user_id} 隐藏了 QQ 等级，将拒绝其加群请求。"
+                        )
+                        await self._handle_request(
+                            event,
+                            request_flag,
+                            False,
+                            self._message_template.get_msg_template(
+                                "GroupRequestReview", "RejectByInvalidLevel"
+                            ),
+                        )
+                        return
+                    logger.debug(
+                        f"用户 {user_id} 隐藏了 QQ 等级，但插件配置中启用了跳过无效等级，将绕过等级限制。"
+                    )
+                elif member_info["qqLevel"] < level_limit["MinLevel"]:
+                    logger.info(
+                        f"用户 {user_id} 等级 {member_info['qqLevel']} 低于要求的最小等级 {level_limit['MinLevel']}，将拒绝其加群请求。"
+                    )
+                    await self._handle_request(
+                        event,
+                        request_flag,
+                        False,
+                        self._message_template.get_msg_template(
+                            "GroupRequestReview",
+                            "RejectByLevelLimit",
+                            required_level=level_limit["MinLevel"],
+                        ),
+                    )
+                    return
+            match = re.search(group_config["Regex"], input_answer)
+            if not match:
+                logger.info(f"用户 {user_id} 答案中未匹配到关键词，将拒绝其加群请求。")
                 await self._handle_request(
                     event,
                     request_flag,
                     False,
                     self._message_template.get_msg_template(
-                        "GroupRequestReview",
-                        "RejectByLevelLimit",
-                        required_level=level_limit["MinLevel"],
+                        "GroupRequestReview", "RejectByRegex"
                     ),
                 )
                 return
-        match = re.search(group_config["Regex"], input_answer)
-        if not match:
-            logger.info(f"用户 {user_id} 答案中未匹配到关键词，将拒绝其加群请求。")
-            await self._handle_request(
-                event,
-                request_flag,
-                False,
-                self._message_template.get_msg_template(
-                    "GroupRequestReview", "RejectByRegex"
-                ),
-            )
-            return
 
-        # 检查全部通过，同意加群请求
-        logger.info(
-            f"用户 {user_id} 答案中匹配了关键词 `{match.group()}`，将同意其加入群 {group_id}。"
-        )
-        await self._handle_request(event, request_flag, True)
-        return
+            logger.info(
+                f"用户 {user_id} 答案中匹配了关键词 `{match.group()}`，将同意其加入群 {group_id}。"
+            )
+            await self._handle_request(event, request_flag, True)
+            return
+        except Exception:
+            logger.exception("处理加群请求时发生错误。")
+            return
 
     @staticmethod
     async def _handle_request(
